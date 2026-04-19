7a485175018fb93c9d33b40975c6bf7bc1fbca10
astr0nom1ca <jeffreyonemu@gmail.com>
2026-04-19 16:25:55 +0100

    Liverpool

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,21 +1,21 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+'use client'
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
+import { useEffect, useState } from 'react'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
-
-export { metadata, viewport } from 'next-sanity/studio'
+// REMOVED: export { metadata, viewport }- Next js cannot read Sanity's metadata hence build error
 
 export default function StudioPage() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return <NextStudio config={config} />
 }
